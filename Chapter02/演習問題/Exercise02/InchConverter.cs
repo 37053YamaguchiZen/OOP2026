5abe89960502dfa5879befa183c801ac6d34547e
山口　漸 <oibc37053@ojs.ac.jp>
2026-05-13 11:31:17 +0900

    Exercise02完成

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -8,7 +8,13 @@ namespace Exercise02 {
     internal class InchConverter {
         private static readonly double ratio = 0.0254;  //定数
 
+        //メートルからインチを求める
         public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+        //インチからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * ratio;
         }
     }
 }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,7 +3,7 @@
         static void Main(string[] args) {
             PrintInchToMeterList(1,10);
         }
-            private static void PrintInchToMeterList(int start, int stop) {
+            static void PrintInchToMeterList(int start, int stop) {
             for (int Inch = start; Inch <= stop; Inch++) {
                 double meter = InchConverter.ToMeter(Inch);
                 Console.WriteLine($"{Inch}Inch = {meter:0.0000}m");
