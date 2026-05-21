c8f31b3fb80d766c5c0e6fefb2ff66fc5017df08
山口　漸 <oibc37053@ojs.ac.jp>
2026-05-21 11:20:34 +0900

    3.1.4完成

--- Chapter03/ラムダ式とLINQ/Exercise01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Exercise01/Program.cs
@@ -41,7 +41,7 @@ namespace Exercise01 {
         }
 
         private static void Exercise4(List<int> numbers) {
-    
-            }
+            numbers.Select(n => n * 2).ToList().ForEach(n => Console.WriteLine(n));
         }
+    }
     }
