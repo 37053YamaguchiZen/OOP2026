8842ede67673995c44a3762467c3c0c85ac236cc
山口　漸 <oibc37053@ojs.ac.jp>
2026-05-28 10:18:55 +0900

    4.2.3

--- Chapter04/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Threading.Channels;
 
 namespace Exercise02 {
     internal class Program {
@@ -11,42 +13,52 @@ namespace Exercise02 {
             Exercise3();
         }
 
-        
+
         private static void Exercise1() {
             var s = Console.ReadLine();
-            if(int.TryParse(s,out var num))
-            if(num < 0) {
-                Console.WriteLine(num);
-            } else if (num < 100) {
-                Console.WriteLine(num * 2);
-            } else if (num < 500) {
-                Console.WriteLine(num * 3);
-            } else {
-                Console.WriteLine(num);
-            }
+            if (int.TryParse(s, out var num))
+                if (num < 0) {
+                    Console.WriteLine(num);
+                } else if (num < 100) {
+                    Console.WriteLine(num * 2);
+                } else if (num < 500) {
+                    Console.WriteLine(num * 3);
+                } else {
+                    Console.WriteLine(num);
+                }
         }
 
         private static void Exercise2() {
             var s = Console.ReadLine();
             if (int.TryParse(s, out var num))
                 switch (num) {
-                case < 0:
-                    Console.WriteLine(num);
-                    break;
-                case < 100:
-                    Console.WriteLine(num * 2);
-                    break;
-                case < 500:
-                    Console.WriteLine(num * 3);
-                    break;
-                default:
-                    Console.WriteLine(num);
-                    break;
-            }
+                    case < 0:
+                        Console.WriteLine(num);
+                        break;
+                    case < 100:
+                        Console.WriteLine(num * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(num * 3);
+                        break;
+                    default:
+                        Console.WriteLine(num);
+                        break;
+                }
         }
 
         private static void Exercise3() {
+            var s = Console.ReadLine();
+            if (int.TryParse(s, out var num)) {
+                var outNum = num switch {
+                    < 0 => num,
+                    < 100 => num * 2,
+                    < 500 => num * 3,
+                => num
 
+                };
+                Console.WriteLine(outNum);
+            }
         }
     }
 }
