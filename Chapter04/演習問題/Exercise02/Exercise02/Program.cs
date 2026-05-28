f87414786b8aa176253c784716df35af4b25b35a
山口　漸 <oibc37053@ojs.ac.jp>
2026-05-28 09:50:05 +0900

    4.2.2

--- Chapter04/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -14,7 +14,7 @@ namespace Exercise02 {
         
         private static void Exercise1() {
             var s = Console.ReadLine();
-            int num = int.Parse(s);
+            if(int.TryParse(s,out var num))
             if(num < 0) {
                 Console.WriteLine(num);
             } else if (num < 100) {
@@ -27,7 +27,22 @@ namespace Exercise02 {
         }
 
         private static void Exercise2() {
-
+            var s = Console.ReadLine();
+            if (int.TryParse(s, out var num))
+                switch (num) {
+                case < 0:
+                    Console.WriteLine(num);
+                    break;
+                case < 100:
+                    Console.WriteLine(num * 2);
+                    break;
+                case < 500:
+                    Console.WriteLine(num * 3);
+                    break;
+                default:
+                    Console.WriteLine(num);
+                    break;
+            }
         }
 
         private static void Exercise3() {
