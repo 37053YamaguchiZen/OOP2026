c0c1157ba74fedb984cefe6a961fa5f57621aaa4
山口　漸 <oibc37053@ojs.ac.jp>
2026-06-01 11:26:16 +0900

    5.1.2

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -10,9 +10,16 @@ namespace Exercise01 {
         public int Year { get; set; }
         public int Month { get; init; }
 
-        public YearMonth(string Year, string Month) {
+        public YearMonth(int Year, int Month) {
             Year = Year;
             Month = Month;
+        
+        }
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
+        //5.1.3
+        public YearMonth AddOneMonth() {
+
+            
         }
     }
 }
