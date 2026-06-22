538bbd3c1c97f729291f855bb7468e2a5bc25653
山口　漸 <oibc37053@ojs.ac.jp>
2026-06-22 11:36:09 +0900

    設問1_提出

--- TestProject/Test01_01/ScoreCounter.cs
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -11,27 +11,28 @@
         private static IEnumerable<Student> ReadScore(string filePath) {
             var sales = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            
-
-
-
-
-
-
-
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var sale = new Student {
+                    name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
             return sales;
         }
 
         //メソッドの概要：
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
-
-
-
-
-
-
-            return dict;
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.name))
+                    dict[sale.name] += sale.Score;
+                else
+                    dict[sale.name] = sale.Score;
+            }
+                return dict;
         }
     }
 }
