a62787eb4465d36ca5d6b15076bebd2f7ae161d4
山口　漸 <oibc37053@ojs.ac.jp>
2026-06-30 15:36:56 +0900

    8.1.2完成

--- chapter08/演習問題/Exercise01/Exercise01/Program.cs
+++ chapter08/演習問題/Exercise01/Exercise01/Program.cs
@@ -24,7 +24,18 @@
                 }
             }
         private static void Exercise2(string text) {
-
+            var dict = new SortedDictionary<char, int>();
+            string upperText = text.ToUpper();
+            foreach (var item in upperText) {
+                if (dict.ContainsKey(item)) {
+                    dict[item] += 1;
+                } else {
+                    dict[item] = 1;
+                }
+            }
+            foreach (var item in dict.OrderBy(x => x.Key)) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
