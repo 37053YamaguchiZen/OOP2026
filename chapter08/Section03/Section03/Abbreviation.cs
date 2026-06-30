45eee2e480d4921b27395b7a1ee4d00fccbd0726
山口　漸 <oibc37053@ojs.ac.jp>
2026-06-30 15:29:47 +0900

    8.1.1完成

---
+++ chapter08/Section03/Section03/Abbreviation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    internal class Abbreviation {
+        private readonly Dictionary<string, string> _dict = new();
+
+        // コンストラクター
+        public Abbreviations() {
+            var lines = File.ReadAllLines("./Abbreviations.txt");
+            _dict = lines
+                .Select(line => line.Split('='))
+                .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        // 要素を追加
+        public void Add(string abbr, string japanese) =>
+            _dict[abbr] = japanese;
+
+        // 省略語から対応する日本語を取り出す
+        public string? Get(string abbr) =>
+            _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+
+        // 日本語から対応する省略語を取り出す
+        public string? ToAbbreviation(string japanese) =>
+            _dict.FirstOrDefault(x => x.Value == japanese).Key;
+
+        // 日本語の部分文字列を引数に与え、それが含まれる要素(Key,Value)をすべて取り出す
+        public IEnumerable<(string, string)> FindAll(string substring) {
+            foreach (var (key, value) in _dict) {
+                if (value.Contains(substring)) {
+                    yield return (key, value);
+                }
+            }
+        }
+    }
+}
---
+++ chapter08/演習問題/Exercise01/Exercise01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+            Exercise2(text);
+        }
+
+        private static void Exercise1(string text) {
+            var dict = new Dictionary<char, int>();
+            string upperText = text.ToUpper();
+
+            foreach (var item in upperText) {
+                if (dict.ContainsKey(item)) {
+                        dict[item] += 1;
+                    } else {
+                        dict[item] = 1;
+                    }
+                }
+            foreach (var item in dict.OrderBy(x => x.Key)) {
+                    Console.WriteLine(item);
+                }
+            }
+        private static void Exercise2(string text) {
+
+        }
+    }
+}
